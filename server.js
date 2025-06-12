1697467ace09718461c137d7bdb91561093e2160
Rakshit Shetty <32040292+rakshit3549@users.noreply.github.com>
2025-06-12 23:31:56 +0530

    cors removed

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const ImageKit = require('imagekit');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.use(cors());
 
 // — ImageKit Setup —
 const imagekit = new ImageKit({
@@ -83,32 +80,3 @@ app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
-
-
-
-
-// require("dotenv").config();
-// const express = require("express");
-// const ImageKit = require("imagekit");
-// const cors = require("cors");
-
-// const app = express();
-// const port = process.env.PORT || 8080;
-
-// app.use(cors()); // Allow frontend requests from browser
-
-// const imagekit = new ImageKit({
-//   publicKey: process.env.PUBLICKEY,
-//   privateKey: process.env.PRIVATEKEY,
-//   urlEndpoint: process.env.URLENDPOINT 
-// });
-
-
-// app.get("/auth", (req, res) => {
-//   const authParams = imagekit.getAuthenticationParameters();
-//   res.json(authParams);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Auth server listening at http://localhost:${port}`);
-// });
